1232a3e7236995c97fc82e516b0b9e2dd24002d7
Kevesha Pillay <keveshap@sahomeloans.com>
2018-10-05 09:16:36 +0200

    Managed to get the Sign in page woring

---
+++ LaVitaLiving/DatabasesClasses/Products.cs
@@ -0,0 +1,12 @@
+﻿using LaVitaLiving.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaVitaLiving.DatabasesClasses
+{
+    public class Products
+    {
+    }
+}
